27680fe5e9d1eccc280d1b33727d7205cb47fb5b
jn-devops <devops@joy-nostalg.com>
2025-01-22 22:25:54 +0800

    add folder model

---
+++ app/Models/Folder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\MediaCollections\MediaCollection;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasMultipleDocuments;
+use Spatie\MediaLibrary\HasMedia;
+
+/**
+ * Class Folder
+ *
+ * @property string $id
+ * @property string $code
+ * @property array $meta
+ *
+ * @method int getKey()
+ * @method Media addDocument(string|\Symfony\Component\HttpFoundation\File\UploadedFile $file)
+ * @method MediaCollection getDocuments()
+ *
+ */
+class Folder extends Model implements HasMedia
+{
+    /** @use HasFactory<\Database\Factories\FolderFactory> */
+    use HasMultipleDocuments;
+    use InteractsWithMedia;
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'code',
+        'meta',
+    ];
+
+    protected $casts = [
+        'meta' => 'array'
+    ];
+
+    /**
+     * This is the pointer to the collection.
+     *
+     * @var string
+     */
+    const COLLECTION_NAME = 'folder-documents';
+
+    /**
+     * This is applicable to .pdf only.
+     *
+     * @var array
+     */
+    const DOCUMENT_MIME_TYPES = [
+        'application/pdf',
+    ];
+}
