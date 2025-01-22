bb6c546632b1acaee0ac1d7e260dc34d6843afbf
jn-devops <devops@joy-nostalg.com>
2025-01-22 13:58:27 +0800

    add set model

---
+++ tests/Unit/SetTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Testing\{RefreshDatabase, WithFaker};
+use App\Models\{Set, Template};
+
+uses(RefreshDatabase::class, WithFaker::class);
+
+test('template model has attributes', function () {
+    $template = Set::factory()->create();
+    expect($template->code)->toBeString();
+    expect($template->name)->toBeString();
+});
+
+test('set model has many template models', function () {
+    $set = Set::factory()->create();
+    expect($set->templates)->toHaveCount(0);
+    [$template1, $template2] = Template::factory(2)->create();
+    $set->templates()->saveMany([$template1, $template2]);
+    $set->refresh();
+    expect($set->templates)->toHaveCount(2);
+});
+
